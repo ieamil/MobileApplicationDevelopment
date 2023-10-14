1af89faf225d031b7479e7f2a18bd6d0d6f009a8
U200709052 <isillimaei@gmail.com>
2023-10-15 02:48:41 +0300

    lab week 2

---
+++ asWeek2/app/src/main/java/com/example/asw2/CustomAdapterActivity.java
@@ -0,0 +1,27 @@
+package com.example.asw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomAdapterActivity extends AppCompatActivity {
+
+    final List<Animal> animalList = new ArrayList<Animal>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_adapter);
+
+        animalList.add(new Animal("Cat",R.mipmap.anime));
+        animalList.add(new Animal("Dog",R.mipmap.anime));
+
+        final ListView listView = findViewById(R.id.listView);
+        AnimalAdapter animalAdapter = new AnimalAdapter(this, animalList);
+        listView.setAdapter(animalAdapter);
+    }
+}
